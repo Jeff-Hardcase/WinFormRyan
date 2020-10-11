571cad2a61ab74e90e8e07e167ffe219e44cd42d
Jeff-Hardcase <32984988+Jeff-Hardcase@users.noreply.github.com>
2020-10-11 15:24:51 -0500

    Add project files.

---
+++ frmTransactions.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinFormRyan
+{
+    partial class frmTransactions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgTransactions = new System.Windows.Forms.DataGridView();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTransactions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgTransactions
+            // 
+            this.dgTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTransactions.Location = new System.Drawing.Point(12, 192);
+            this.dgTransactions.Name = "dgTransactions";
+            this.dgTransactions.Size = new System.Drawing.Size(1127, 518);
+            this.dgTransactions.TabIndex = 0;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(12, 31);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(175, 20);
+            this.txtType.TabIndex = 1;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(12, 15);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(70, 13);
+            this.lblType.TabIndex = 2;
+            this.lblType.Text = "Filter by Type";
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(207, 31);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(95, 23);
+            this.btnFilter.TabIndex = 3;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(348, 31);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmTransactions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 722);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.dgTransactions);
+            this.Name = "frmTransactions";
+            this.Text = "Bitcoin Transaction (new)";
+            this.Load += new System.EventHandler(this.frmTransactions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgTransactions)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgTransactions;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
---
+++ frmTransactions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormRyan
+{
+    public partial class frmTransactions : Form
+    {
+        public frmTransactions()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTransactions_Load(object sender, EventArgs e)
+        {
+            BindTransactionsGrid();
+        }
+
+        private void BindTransactionsGrid(string typeFilter = null)
+        {
+            var connString = Properties.Settings.Default.TransactBitConnectionString;
+            var sqlText = @"SELECT [Time (UTC)]
+                          ,[Type]
+                          ,[Symbol]
+                          ,[Specification]
+                          ,[USD Amount]
+                          ,[Trading Fee (USD)]
+                          ,[USD Balance]
+                          ,[BTC Amount]
+                          ,[BTC Balance]
+                          ,[ETH Amount]
+                          ,[ETH Balance]
+                      FROM[TransactBit].[dbo].[Transactions]";
+
+            SqlParameter sqlParameter = null;
+
+            if(!string.IsNullOrEmpty(typeFilter))
+            {
+                sqlText += " WHERE [Type] = @Type";
+
+                sqlParameter = new SqlParameter
+                {
+                    ParameterName = "@Type",
+                    Value = typeFilter
+                };
+            }
+
+            var conn = new SqlConnection(connString);
+            var cmd = new SqlCommand(sqlText, conn);
+
+            cmd.CommandType = CommandType.Text;
+
+            if (sqlParameter != null)
+                cmd.Parameters.Add(sqlParameter);
+
+            var da = new SqlDataAdapter(cmd);
+            var ds = new DataSet();
+
+            da.Fill(ds);
+
+            dgTransactions.DataSource = ds.Tables[0];
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            BindTransactionsGrid(txtType.Text);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            BindTransactionsGrid();
+        }
+    }
+}
